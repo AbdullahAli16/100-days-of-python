136639ab607a755bddd043f948b277b7857aa7f2
Abdullah Ali <abdullah139200481@gmail.com>
2025-07-31 10:32:51 +0500

    Delete sample.py

--- sample.py
+++
@@ -1,9 +0,0 @@
-def wassup():
-    print("Hey Brother, What's up ?")
-
-Andrew= "The one who lost gwen"
-
-print(__name__)
-
-if __name__=="__main__":      # The content in this section will only be executed when this file is run directly
-    wassup()                    # not impoted and used by any other file
