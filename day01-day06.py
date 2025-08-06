1f4b885b6e9a354bed7299d79c56e7efa4b25de1
Abdullah Ali <abdullah139200481@gmail.com>
2025-08-06 19:31:55 +0500

    Minor updates

--- day01-day06.py
+++ day01-day06.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Day 4 
 
 print("Hello world")
@@ -15,21 +14,3 @@ print("bye")
     and some functions like typeof() 
     "In Python, EVERYTHING is an object"'''
     
-=======
-#Day 4 
-
-print("Hello world")
-
-#Day 5
-print("hello world \"yeh meri dunya\" just kidding") #Used for using double quoted in strings
-
-print("Hey",1,902, "hello again" , sep="&" , end=" ") #separator and end are added in the end 
-print("bye")
-
-#Day 6
-
-'''Intro to Data types and variables
-    and some functions like typeof() 
-    "In Python, EVERYTHING is an object"'''
-    
->>>>>>> e23d7bf6f4021c6deb0956d47a6cd73c70ecaa2a
