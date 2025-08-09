3b180cded8ee8ebebb519fbc56956dbd2af781f7
Abdullah Ali <abdullah139200481@gmail.com>
2025-08-09 22:00:13 +0500

    Minor changes in day27.py and corrected day55.py

--- day27.py
+++ day27.py
@@ -1,7 +1,6 @@
 # Day: 27(Ex: 03 "Kaun baega Crorepati")
 
 import random
-# name= input("Enter your name: ")
 questions=[
             ["Name the tallest mountain peak in the world",
             "Mount Everest","K2","Mauna Kea","Kangchenjunga",1],
@@ -14,35 +13,47 @@ prize_amount=[100,1000,10000]
 
 i=0 # Variable for iterating
 random.shuffle(questions)
+name= input("Hello, user. Please enter your name: ")
+print(f"Welcome to Kaun Banega Crorepati: {name}. You will be asked several questions and after each question \
+you can take the money prize that you have won and leave, or you can compete for a bigger prize. So are \
+you ready ?")
+
+play_cond=input(" Type 'yes' to begin: ")
+play_cond=play_cond.strip().lower()
 
 
-for index , question in enumerate(questions):
-        print(f"\n{question[0]}")
-        print(f"1- {question[1]:<15} 2- {question[2]:<15}")
-        print(f"3- {question[3]:<15} 4- {question[4]:<15}\n")
-        answer=int(input("Enter your answer (1-4): "))
-        # print(answer) 
-        
-        if answer == question[-1]:
-            if len(questions)-1==index:
-                print(f"Congratulations, You have won the highest prize of '{prize_amount[i]}' Rs.\n Have a Great Day :)")
-                break
+if play_cond=="yes":
+    for index , question in enumerate(questions):
+            print(f"\n{question[0]}")
+            print(f"1- {question[1]:<15} 2- {question[2]:<15}")
+            print(f"3- {question[3]:<15} 4- {question[4]:<15}\n")
+            answer=int(input("Enter your answer (1-4): "))
+            
+            
+            if answer == question[-1]:
+                if len(questions)-1==index:
+                    print(f"Congratulations, You have won the highest prize of '{prize_amount[i]}' Rs.\n Have a Great Day :)")
+                    break
+                else:
+                    pass
+                print(f"Your answer is Correct. You have won {prize_amount[i]} Rs.\n")
+                print(f"Do you want to go continue or leave now with your {prize_amount[i]} Rs ?")
+                cont_cond= str(input("What's your answer ? (Yes/No): "))
+                cont_cond=cont_cond.strip().lower()
+                if (cont_cond == "no"):
+                    print(f"Well, congrats on winning your {prize_amount[i]} Rs. Goodbye.")
+                    break
+                elif(cont_cond == "yes"):
+                    pass 
+                else:
+                    print("Wrong input, so quiting anyways ...")
+                    break
+                i=i+1
             else:
-                pass
-            print(f"Your answer is Correct. You have won {prize_amount[i]} Rs.\n")
-            print(f"Do you want to go continue or leave now with your {prize_amount[i]} Rs ?")
-            cont_cond= str(input("What's your answer ? (Yes/No): "))
-            cont_cond=cont_cond.strip().lower()
-            if (cont_cond == "no"):
-                print(f"Well, congrats on winning your {prize_amount[i]} Rs. Goodbye.")
-                break
-            elif(cont_cond == "yes"):
-                pass 
-            else:
-                print("Wrong input, so quiting anyways ...")
-                break
-            i=i+1
-        else:
-            print("Oops, your answer is Incorrect.")    
+                print("Oops, your answer is Incorrect.")    
+
+else:
+    print("You answered something else than 'yes' so, it seems like you are busy at the moment. No worries \
+let's play again soon :)")
     
     
--- day55.py
+++ day55.py
@@ -1,4 +1,54 @@
 # Day: 55(Ex: 05 "Snake, Water, Gun game")
 
+import random
 
-# game=
+# Game Outcome graph:
+'''game=[             Snake  Water  Gun
+            Snake    [Draw,  Snake, Gun]
+            Water    [Snake, Draw,  Water]
+            Gun      [Gun,   Water, Draw]
+    ]'''
+
+def check_outcome(cpu_answer,user_answer):
+    # If CPU chooses "Snake":
+    if  cpu_answer=="snake" and user_answer=="snake":
+        print ("It's a DRAW. No one wins.")
+    elif cpu_answer=="snake" and user_answer=="water":
+        print ("Snake DRINKS the water. CPU wins !")
+    elif cpu_answer=="snake" and user_answer=="gun":
+        print ("Gun KILLS the Snake. Player wins !")
+
+    # If CPU chooses "Water":
+    elif cpu_answer=="water" and user_answer=="snake":
+        print ("Snake DRINKS the water. Player wins !")
+    elif cpu_answer=="water" and user_answer=="water":
+        print ("It's a DRAW. No one wins.")
+    elif cpu_answer=="water" and user_answer=="gun":
+        print ("Water SABOTAGES the gun. CPU wins !")
+    
+    # If CPU chooses "Gun":
+    elif cpu_answer=="gun" and user_answer=="snake":
+        print ("Gun KILLS the Snake. CPU wins !")
+    elif cpu_answer=="gun" and user_answer=="water":
+        print ("Water SABOTAGES the gun. Player wins !")
+    elif cpu_answer=="gun" and user_answer=="gun":
+        print ("It's a DRAW. No one wins.")
+        
+
+options= ["Snake","Water","Gun"]
+cpu_answer=random.choice(options)
+cpu_answer=cpu_answer.strip().lower()
+print(cpu_answer)
+
+print("Welcome to the Snake, Water and Gun game. The rules are basic, the Snake drinks the water, water \
+sabotages the gun and the gun kills the snake. Shall we begin ?")
+
+play_cond=input("Type 'yes' to play: ")
+play_cond=play_cond.strip().lower()
+
+if play_cond=="yes":
+    user_answer=str(input("Snake,water or gun ? (Before the time runs out, type ONE): "))
+    user_answer=user_answer.strip().lower()
+    check_outcome(cpu_answer,user_answer)
+else:
+    print("It seems like you don't want to play right now, let's play sometime else soon")
